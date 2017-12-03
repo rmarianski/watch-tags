efa10130eb1604b99bb1b0a0f2fbb122615bb5a5
Robert Marianski <rob@marianski.com>
2017-12-03 11:19:34 -0500

    Refactor to use link abstraction

--- watch-tags.c
+++ watch-tags.c
@@ -10,15 +10,16 @@
 #include <sys/inotify.h>
 #include <pthread.h>
 
-typedef struct {
+typedef struct watch_path_s {
     char *path;
     int fd;
+    struct watch_path_s *base;
 } watch_path_s;
 
-typedef struct {
-    watch_path_s *watch_paths;
-    unsigned int n_used;
-} watch_paths_s;
+typedef struct watch_link_s {
+    watch_path_s *wp;
+    struct watch_link_s *next;
+} watch_link_s;
 
 typedef enum {
     ProcState_None,
@@ -27,28 +28,65 @@ typedef enum {
 } proc_state_s;
 
 typedef struct {
-    watch_paths_s queue;
+    watch_link_s *first_queue;
     proc_state_s proc_state;
     unsigned int wait_time;
     char *cmd;
 } queue_state_s;
 
-void add_watch_path(watch_paths_s *dps, watch_path_s *wp) {
-    for (unsigned int dp_idx = 0; dp_idx < dps->n_used; dp_idx++) {
-        watch_path_s *dp = dps->watch_paths + dp_idx;
-        if (dp->fd == wp->fd) {
-            return;
-        }
-    }
-    watch_path_s *dp = dps->watch_paths + dps->n_used++;
-    *dp = *wp;
+typedef struct {
+    watch_link_s *first_watched;
+    watch_link_s *first_dirty;
+    watch_link_s *first_queue;
+
+    queue_state_s queue_state;
+
+    watch_link_s *first_free;
+} watch_state_s;
+
+watch_path_s *alloc_watch_path(watch_state_s *state) {
+    watch_path_s *result = malloc(sizeof(watch_state_s));
+    result->fd = 0;
+    result->path = 0;
+    result->base = 0;
+    return result;
 }
 
-bool enqueue_watch_paths(queue_state_s *qs, watch_paths_s *dps) {
+watch_link_s *alloc_link(watch_state_s *state) {
+    watch_link_s *result;
+    if (state->first_free) {
+        result = state->first_free;
+        state->first_free = state->first_free->next;
+    } else {
+        result = malloc(sizeof(watch_link_s));
+    }
+    result->next = 0;
+    result->wp = 0;
+    return result;
+}
+
+void free_link(watch_state_s *state, watch_link_s *link) {
+    link->next = state->first_free;
+    state->first_free = link;
+}
+
+watch_link_s *add_watch_path_if_needed(watch_state_s *state, watch_link_s *dest_list, watch_path_s *wp) {
+    // first check if in list already
+    for (watch_link_s *l = dest_list; l; l = l->next) {
+        if (l->wp->fd == wp->fd) {
+            return dest_list;
+        }
+    }
+    watch_link_s *new_link = alloc_link(state);
+    new_link->wp = wp;
+    new_link->next = dest_list;
+    return new_link;
+}
+
+bool enqueue_watch_paths(watch_state_s *state, queue_state_s *qs, watch_link_s *first_dirty) {
     if (__sync_bool_compare_and_swap(&qs->proc_state, ProcState_None, ProcState_Enqueueing)) {
-        for (unsigned int i = 0; i < dps->n_used; i++) {
-            watch_path_s *wp = dps->watch_paths + i;
-            add_watch_path(&qs->queue, wp);
+        for (watch_link_s *l = first_dirty; l; l = l->next) {
+            qs->first_queue = add_watch_path_if_needed(state, qs->first_queue, l->wp);
         }
         qs->proc_state = ProcState_None;
         return true;
@@ -65,20 +103,29 @@ void path_changed(queue_state_s *qs, char *path) {
     pclose(pipe);
 }
 
+void free_links(watch_state_s *state, watch_link_s *first_link) {
+    watch_link_s *l = first_link;
+    while (l) {
+        watch_link_s *next = l->next;
+        free_link(state, l);
+        l = next;
+    }
+}
+
 void *process_queue(void *pthread_data) {
-    queue_state_s *qs = pthread_data;
-    watch_paths_s *queue = &qs->queue;
+    watch_state_s *state = pthread_data;
+    queue_state_s *qs = &state->queue_state;
     for (;;) {
         while (!__sync_bool_compare_and_swap(&qs->proc_state, ProcState_None, ProcState_Processing)) {
             perr_die_if(pthread_yield() != 0,  "pthread_yield");
         }
 
-        for (unsigned int i = 0; i < queue->n_used; i++) {
-            watch_path_s *wp = queue->watch_paths + i;
-
-            path_changed(qs, wp->path);
+        for (watch_link_s *l = qs->first_queue; l; l = l->next) {
+            path_changed(qs, l->wp->path);
         }
-        queue->n_used = 0;
+        free_links(state, qs->first_queue);
+
+        qs->first_queue = NULL;
         qs->proc_state = ProcState_None;
 
         sleep(qs->wait_time);
@@ -103,18 +150,10 @@ int main(int argc, char *argv[]) {
         die_usage(argv[0], "<watch-path>");
     }
 
-    unsigned int n_watch_paths = argc - 1;
-    unsigned int mem_size = n_watch_paths * sizeof(watch_path_s) * 3;
-    void *memory = malloc(mem_size);
-    memset(memory, 0, mem_size);
-
-    watch_path_s *watch_paths = memory;
-    watch_path_s *dirty_watch_paths = watch_paths + n_watch_paths;
-    watch_path_s *queue_watch_paths = dirty_watch_paths + n_watch_paths;
+    watch_state_s state = {0};
 
     int inotify_fd = inotify_init();
     perr_die_if(inotify_fd < 0, "inotify_init");
-    unsigned int watch_path_idx = 0;
     for (unsigned int arg_idx = 1; arg_idx < argc; arg_idx++) {
         char *watch_path = argv[arg_idx];
         int watch_fd = inotify_add_watch(
@@ -123,27 +162,28 @@ int main(int argc, char *argv[]) {
             IN_MODIFY | IN_CREATE | IN_DELETE | IN_MOVE
         );
         perr_die_if(watch_fd < 0, "inotify_add_watch");
-        watch_path_s *watched_path = watch_paths + watch_path_idx++;
-        watched_path->fd = watch_fd;
-        watched_path->path = watch_path;
+        watch_path_s *wp = alloc_watch_path(&state);
+        wp->fd = watch_fd;
+        wp->path = watch_path;
+
+        watch_link_s *link = alloc_link(&state);
+        link->wp = wp;
+        link->next = state.first_watched;
+        state.first_watched = link;
     }
 
-    watch_paths_s dirty_paths = {0};
-    dirty_paths.watch_paths = dirty_watch_paths;
-
-    queue_state_s queue_state = {0};
-    queue_state.queue.watch_paths = queue_watch_paths;
+    queue_state_s *queue_state = &state.queue_state;
     char *wait_time_str = getenv("WATCHTAGS_WAIT_TIME");
-    queue_state.wait_time = parse_wait_time(wait_time_str, 60);
+    queue_state->wait_time = parse_wait_time(wait_time_str, 60);
     char *cmd = getenv("WATCHTAGS_CMD");
     if (cmd) {
-        queue_state.cmd = cmd;
+        queue_state->cmd = cmd;
     } else {
-        queue_state.cmd = "ctagspath";
+        queue_state->cmd = "ctagspath";
     }
 
     pthread_t thread;
-    perr_die_if(pthread_create(&thread, NULL, process_queue, &queue_state) != 0, "pthread_create");
+    perr_die_if(pthread_create(&thread, NULL, process_queue, &state) != 0, "pthread_create");
 
     char inotify_buf[4096];
 
@@ -157,18 +197,18 @@ int main(int argc, char *argv[]) {
                 // otherwise we'll get stuck in a loop!
                 continue;
             }
-            for (unsigned int wp_idx = 0; wp_idx < n_watch_paths; wp_idx++) {
-                watch_path_s *watched_path = watch_paths + wp_idx;
-                if (e->wd == watched_path->fd) {
-                    add_watch_path(&dirty_paths, watched_path);
+            for (watch_link_s *l = state.first_watched; l; l = l->next) {
+                if (e->wd == l->wp->fd) {
+                    state.first_dirty = add_watch_path_if_needed(&state, state.first_dirty, l->wp);
                     break;
                 }
             }
         }
-        while (!enqueue_watch_paths(&queue_state, &dirty_paths)) {
+        while (!enqueue_watch_paths(&state, queue_state, state.first_dirty)) {
             perr_die_if(pthread_yield() != 0,  "pthread_yield");
         }
-        dirty_paths.n_used = 0;
+        free_links(&state, state.first_dirty);
+        state.first_dirty = NULL;
     }
 
     return 0;
